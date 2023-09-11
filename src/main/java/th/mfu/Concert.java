5d144c9fcac593e2a27d4af667d0b756bfeb3521
Code036 <119183720+Code036@users.noreply.github.com>
2023-09-11 10:19:10 +0000

    week5-036

--- src/main/java/th/mfu/Concert.java
+++ src/main/java/th/mfu/Concert.java
@@ -5,22 +5,60 @@ import java.util.Date;
 public class Concert {
 
     //TODO: add attributes
+    private int id;
+    private String title;
+    private String description;
+    private String performer;
+    private Date date;
+    
 
+
+    public Date getDate() {
+        return date;
+    }
+    public void setDate(Date date) {
+        this.date = date;
+    }
     public Concert() {
     }
     public Concert(String title, String description) {
         //TODO: set attributes
+        this.title = title;
+        this.description = description;
     }
  
     public String getTitle() {
         //TODO: return title
-        return "";
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
     }
     public String getDescription() {
         //TODO: return description
-        return "";
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getPerformer() {
+        return performer;
+    }
+    public void setPerformer(String performer) {
+        this.performer = performer;
     }
 
+
+    
+
+
     //TODO: add getters and setters
+    
 
 }
